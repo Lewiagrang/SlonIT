08264c01f9bd48dd54685d1dd55026232dcefc85
Lewiagrang <166782741+Lewiagrang@users.noreply.github.com>
2025-01-26 18:16:54 +0300

    Create objects(Задание №10)

---
+++ objects.js
@@ -0,0 +1,27 @@
+const sumOfNumbers = (obj) => {
+    let accumulator = 0
+    for(let prop in obj){
+        if(typeof obj[prop] === "number"){
+            accumulator+=obj[prop]
+        }
+    }
+    return accumulator
+}
+
+const extraCreditQuestion = () => {
+    const result = []
+    const arrNumbers = [] 
+    for(let prop in obj){
+        if(typeof obj[prop] === "number"){
+            arrNumbers.push({[prop]: obj[prop], prop: obj[prop]})
+        }
+    }
+
+    arrNumbers.sort((a, b) => a.prop > b.prop ? -1 : 1);
+
+    for(let i = 0; i<arrNumbers.length; i++){
+        result.push(...Object.keys(arrNumbers[i])[0])
+    }
+    return result
+}
+
